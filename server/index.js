09b7992dba9dec8014aeb27d664a95cae149be1b
Jan Korczak <jankorczak@Jans-MacBook-Air.local>
2023-05-22 00:21:22 +0200

    backend done

---
+++ server/index.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const mongoose = require('mongoose');
+const User = require('./models/user.model');
+require("dotenv").config();
+
+app.use(cors())
+app.use(express.json())
+
+mongoose.connect(process.env.MONGO_URL);
+
+app.post('/api/register', async (req,res) => {
+    try {
+        await User.create({
+            name: req.body.name,
+            email: req.body.email,
+            password: req.body.password,
+        })
+        res.json({ status: 'ok' })
+    } catch (err) {
+        res.json({ status: 'error', error: 'Duplicate email' })
+    }
+})
+
+app.post('/api/login', async (req,res) => {
+        const user = await User.findOne({ email: req.body.email, password: req.body.password });
+        if(user) {
+            return res.json({ status: 'ok', user: true })
+        } else {
+            return res.json({ status: 'error', user: false })
+        }
+})
+
+app.listen(1337, () => {
+    console.log('Server started on 1337');
+})
